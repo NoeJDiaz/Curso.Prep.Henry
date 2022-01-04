5dc6e88fd13b6d0d0f6d8117a16cafbcf756f9c9
NoeJDiaz <diaz.noelia.j@gmail.com>
2022-01-04 16:46:27 -0300

    finish homework 1

---
+++ 02-JS-I/homework/Untitled-1.rb
@@ -0,0 +1,5 @@
+Variable: Un lugar en donde se guarda un valor de algo que se va a usar mas tarde.
+Strings: Conjunto de numeros y/o letras, que se ecriben entre comillas.
+Funciones (argumentos, return): Las variables pueden convertirse en funciones para poder combinarlas,sumarlas, restarlas entre si, las mismas reciben un argumento el que va entre parentesis,y estas nos van a retornar algo como resultado de su uso, lo cual se denomina return.
+Declaraciones if : Es un operador para certificar si algo es verdadero, retorne algo esspecifico.
+Valores booleanos (true, false): Codigo binario que puede tomar el valor de 0 o 1, en este caso se utilizan los strings "true" y "false".
